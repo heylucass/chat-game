2f85e21eaea790389aa0c52fa08bcebb08e9860a
Lucas Nasc <lucassnascimentos4@gmail.com>
2022-01-28 19:58:08 -0300

    fix: delete msmbox.jsx

--- Components/DisplayBox/DisplayBox.jsx
+++ Components/DisplayBox/DisplayBox.jsx
@@ -1,11 +1,18 @@
-import MsmBox from "../MsmBox/MsmBox"
+//import MsmBox from "../MsmBox/MsmBox"
 export default function DisplayBox(props) {
     const array = props.list;
     
     return (<>
         <div id="display" className="display">{array.map((msmUpdated) => {
             return (
-                <MsmBox updated={msmUpdated}/>
+                <div key={msmUpdated.id} className="msm-Container">
+                <span>
+                    <img src={`https://github.com/${msmUpdated.de}.png`} alt="fotu" />
+                    <p className="msm-Owner">{msmUpdated.de}</p>
+                    <p className="msm-Date">{(new Date().toLocaleDateString())}</p>
+                </span><br />
+                <h5 className="msm-Content">{msmUpdated.texto}</h5><br />
+            </div>
             )
         })}
         </div>
@@ -25,6 +32,50 @@ export default function DisplayBox(props) {
                 .display::-webkit-scrollbar-thumb {
                     background: red;
                     border-radius: 10px;
-                }`}</style>
+                }
+                .msm-Container {
+                    width: 98%;
+                    height: 30%;
+                    display: flex;
+                    flex-direction: column;
+                    margin-left: 4px;
+                    transition-duration: 0.5s;
+                    margin-bottom: 5px;
+                    justify-content: center ;
+                    align-items: start;  
+                }
+                .msm-Container:hover {
+                    background-color: black;
+                    border-radius: 5px;
+                }
+                span {
+                    display: flex;
+                    flex-direction: row;
+                    height: 1px;
+                    margin-left: 4px;
+                }
+                img {
+                    border-radius: 50%;
+                    width: 25px;
+                    height: 25px;
+                    margin-right: 5px;
+                }
+                .msm-Owner {
+                  margin-right: 15px;
+                  height: 12px;
+                  margin-top: 2px;
+                }
+                .msm-Date {
+                  font-size: 0.7rem;
+                  color: grey;
+                  margin-top: 7px;
+                  margin-bottom: 10px;
+                }
+                .msm-Content {
+                    height: 12px;
+                    margin-left: 4px;
+                    margin-top: 5px;
+                }
+                    `}</style>
     </>)
 }
